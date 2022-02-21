17d2ed55c5d000fdd7edaf6f2017018f7d130617
Brittank88 <masonhunt130ii@gmail.com>
2022-02-21 11:45:56 +1100

    Logging fix
    + Resources folder name case fix

--- src/main/java/com/brittank88/dmca/DMCA.java
+++ src/main/java/com/brittank88/dmca/DMCA.java
@@ -1,8 +1,13 @@
 package com.brittank88.dmca;
 
 import net.fabricmc.api.ModInitializer;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class DMCA implements ModInitializer {
+
+    public static final Logger LOGGER = LogManager.getLogger("DMCA");
+
     @Override
-    public void onInitialize() { System.out.println("Initializing DMCA!"); }
+    public void onInitialize() { LOGGER.info("DMCA initialized!"); }
 }
